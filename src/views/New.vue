42f14004912e38adf33486f52596fb1c790144da
NaokiDohi <1210113.naoki.dohi.913@gmail.com>
2020-09-02 07:59:26 +0900

    New component is created.

---
+++ src/views/New.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="editor">
+    <h1>New Memo</h1>
+    <textarea name="memo" v-model="memoBody"></textarea>
+    <button @click="save">Save</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "new",
+  data: function() {
+    return {
+      memoBody: "",
+    };
+  },
+  methods: {
+    save: function() {
+      alert("This memo is saved.");
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 10em;
+}
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+</style>
